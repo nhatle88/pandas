17ebc55df992f5371bad2ca64529fd25558a0c09
Nhat Le <lehoangnhat88@gmail.com>
2021-04-14 14:14:09 +0700

    Create export_to_excel.py

---
+++ export_to_excel.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import base64
+from io import BytesIO
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
+
+df = ... # your dataframe
+st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+# PS: pip install xlsxwriter  # pandas need this
+# Source: from the internet
